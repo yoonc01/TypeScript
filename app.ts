cd5908000c70eeff61509dbca11cc428bfa1b24d
yoonc01 <yoonc01@hotmail.com>
2025-02-17 14:01:35 +0900

    feat: add type in app.ts

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+// unintended behavior occurs
+// by using ts we can prevent it!
+// however compile goes on
+//const number1 = "5";
+const number1 = 5;
+const number2 = 2.9;
+
+const result = add(number1, number2);
+console.log(result);
