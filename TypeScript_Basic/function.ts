0002bf9bdc730bf0a54721fe26f83701fb05c547
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-22 23:06:17 +0900

    feat: setting a Type of variable that stores function

--- TypeScript_Basic/function.ts
+++ TypeScript_Basic/function.ts
@@ -1,12 +1,13 @@
 //return type
+
+// refer string
+//function add(n1: number, n2: number) {
+    //return n1.toString() + n2.toString();
+//}
+
 // refer number
 function add(n1: number, n2: number) {
     return n1 + n2;
-}
-
-// refer string
-function add(n1: number, n2: number) {
-    return n1.toString() + n2.toString();
 }
 
 // 명시적으로 반환 타입 설정 가능
@@ -27,3 +28,18 @@ function printResult1(num: number): undefined {
     console.log("Result: " + num);
     return ;
 }
+
+// any로 설정된 combineValues는 함수가 아닌 다른 값이 들어갈 수 있어 runtime error 발생 가능성이 있다
+// let combineValues;
+
+// Function으로 타입을 설정하면 이제 함수만 넣을 수 있지만 반환 타입까지 검사해주지는 않는다.
+// let combineValues: Function;
+
+// () 안의 타입까지 맞아야 한다!
+let combineValues: (a: number, b: number) => number;
+
+// 오류가 발생한다!
+// combineValues = printResult;
+
+combineValues = add;
+console.log(combineValues(1, 2));
